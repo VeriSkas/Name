0009dd6460a903ee6657ced03ecda49b20924338
VeriSkas <92_sveto4ka@mail.ru>
2021-04-02 14:15:51 +0300

    dz13

--- index.js
+++ index.js
@@ -1,59 +1,38 @@
-const result = document.getElementById('result');
-const resultSqrt = document.getElementById('resultSqrt');
-const resultDivisor = document.getElementById('resultDivisor');
-const resultPercent = document.getElementById('resultPercent');
+let arr1 = [];
+let arr2 = [];
 
+for (i = 0; i < 10; i++) {
+    arr1[i] = Math.floor(Math.random() * 1000);
+    arr2[i] = Math.floor(Math.random() * 1000);
+}
 
-function insert(num) {
-    document.form.textview.value = document.form.textview.value + num;
-    result.innerHTML = document.form.textview.value;
-}
-function minus() {
-    document.form.textview.value = -document.form.textview.value;
-    result.innerHTML = document.form.textview.value;
-}
-function sqrt() {
-    let inpValue = document.form.textview.value;
-    let inpSqrt = Math.sqrt(document.form.textview.value);
-    resultSqrt.innerHTML = `&radic;(${inpValue}) = ${inpSqrt}`;
-    document.form.textview.value = Math.sqrt(document.form.textview.value);
-}
-function divisor() {
-    let inpValue = document.form.textview.value;
-    let inpDivisor = 1/(document.form.textview.value);
-    resultDivisor.innerHTML = `1/(${inpValue}) = ${inpDivisor}`;
-    document.form.textview.value = 1/(document.form.textview.value);
-}
-function clean() {
-    document.form.textview.value = "";
-}
-function back() { // .substring(start [, end]) Возвращает часть строки между start и end.
-    let inpBack = document.form.textview.value;
-    document.form.textview.value = inpBack.substring(0, inpBack.length-1);
-    result.innerHTML = document.form.textview.value;
-}
-function percent() {
-    let inpPercent = document.form.textview.value;
-    let stringBeforePercent = Number(inpPercent.substring(0, inpPercent.length - 3));
-    let stringPercent = Number(inpPercent.substring(inpPercent.length - 3));
-    let i = 2;
+console.log(arr1, arr2);
 
-    while (isNaN(stringPercent) || isNaN(stringBeforePercent)) {
-        stringBeforePercent = Number(inpPercent.substring(0, inpPercent.length - i));
-        stringPercent = Number(inpPercent.substring(inpPercent.length - i));
-        i++;
+function transformArray (oneArray, twoArray) {
+    let maxNumber = oneArray[1];
+    let minNumber = oneArray[1];
+
+    oneArray = oneArray.concat(twoArray);
+    console.log(oneArray);
+    oneArray.push(Math.floor(Math.random() * 1000), Math.floor(Math.random() * 1000));
+    console.log(oneArray);
+
+    for (i = 0; i < 11; i++) {
+        oneArray[2 * i] = oneArray[2 * i] * 2;
     }
 
-    if (stringBeforePercent == 0) {
-        document.form.textview.value = eval(stringPercent / 100);
-    } else {
-        document.form.textview.value = eval(((100 + stringPercent) * stringBeforePercent) / 100);
+    console.log(oneArray);
+
+    for (i = 0; i < 11; i++) {
+        if (oneArray[i] > maxNumber) {
+            maxNumber = oneArray[i];
+        }
+        if (oneArray[i] < minNumber) {
+            minNumber = oneArray[i];
+        }
     }
-    resultPercent.innerHTML = `${inpPercent}% = ${document.form.textview.value}`;
+
+    console.log(oneArray, maxNumber, minNumber)
 }
-function equal() {
-    let inpValue = document.form.textview.value;
-    let inpEqual = eval(document.form.textview.value); // eval выполняет значение в скобках
-    result.innerHTML = `${inpValue} = ${inpEqual}`;
-    document.form.textview.value = eval(document.form.textview.value);
-}
+
+transformArray(arr1, arr2);
