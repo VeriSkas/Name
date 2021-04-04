da5c25764361cb1b0cc971fa2fc240df368b72f7
VeriSkas <92_sveto4ka@mail.ru>
2021-04-04 18:27:17 +0300

    dz14

--- index.js
+++ index.js
@@ -1,39 +1,105 @@
-let arr1 = [];
-let arr2 = [];
+// _______________1_________________
 
-for (i = 0; i < 10; i++) {
-    arr1[i] = Math.floor(Math.random() * 1000);
-    arr2[i] = Math.floor(Math.random() * 1000);
+// let arr = [1, 2, 3, 4, 5,];
+// arr.splice(1, 2);
+// console.log(arr);
+
+// _______________2_________________
+
+// let str = 'My favorite "Hello world"';
+// let arr = str.split('').reverse();
+// let reverseStr = arr.join('');
+// console.log(reverseStr);
+
+// ______________3__________________
+
+// let str = 'My favorite "Hello world"';
+// let arr = str.split('');
+// let reverseArr = [];
+
+// for (i = arr.length - 1; i >= 0; i--) {
+//     reverseArr.push(arr[i]);
+// }
+
+// let reverseStr = reverseArr.join('');
+// console.log(reverseStr);
+
+// ______________4____________________
+
+// let arr = ['Hello', 4, 99, 0, false, 'world', null];
+// let arr1 = ['Hello', 4, 99, 0, false, 'world', null];
+// let arr2 = ['Hello', 4, 100, 0, true, 'world', null, ''];
+
+// function compareArr (a, b) {
+//     if (a.length === b.length) {
+//         for (i = 0; i < a.length; i++){
+//             if  (a[i] !== b[i]) {
+//                 console.log('false');
+//                 return false;
+//             }
+//         }
+//         console.log('true');
+//         return true;
+//     } else {
+//         console.log('false');
+//         return false;
+//     }
+// }
+
+// compareArr(arr, arr1);
+// compareArr(arr, arr2);
+
+// _________________5___________________
+
+// for (i = 1; i <= 100; i++) {
+//     if ((i % 3 === 0) && (i % 5 === 0)) {
+//         console.log('FizzBuzz');
+//     } else if (i % 3 === 0) {
+//         console.log('Fizz');
+//     } else if (i % 5 === 0) {
+//         console.log('Buzz');
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+// _________________6____________________
+
+// let arr = [];
+// let arr1 = [];
+// let lengthArr = 8;
+
+// for (i = 0; i < lengthArr; i++) {
+//     if (i % 2 === 0) {
+//         arr[i] = '#';
+//         arr1[i] = ' ';
+//     } else {
+//         arr[i] = ' ';
+//         arr1[i] = '#';
+//     }
+// }
+
+// for (i = 0; i < lengthArr; i++) {
+//     (i % 2 === 0) ? console.log(arr.join('')): console.log(arr1.join(''));
+// }
+
+// _________________7_________________
+
+let input = document.getElementById('input');
+let list = document.getElementById('list');
+
+function inputList() {
+    let newListItem = document.createElement('li');
+    newListItem.textContent = input.value;
+    list.appendChild(newListItem);
 }
 
-console.log(arr1, arr2);
-
-function transformArray (oneArray, twoArray) {
-    let maxNumber = oneArray[0];
-    let minNumber = oneArray[0];
-
-    oneArray = oneArray.concat(twoArray);
-    console.log(oneArray);
-    oneArray.push(Math.floor(Math.random() * 1000), Math.floor(Math.random() * 1000));
-    console.log(oneArray);
-
-    for (i = 0; i < 11; i++) {
-        oneArray[2 * i] = oneArray[2 * i] * 2;
-    }
-
-    console.log(oneArray);
-
-    for (i = 0; i < oneArray.length; i++) {
-        if (oneArray[i] > maxNumber) {
-            maxNumber = oneArray[i];
-        }
-        if (oneArray[i] < minNumber) {
-            minNumber = oneArray[i];
-        }
-    }
-    
-    oneArray = oneArray.sort((a, b) => b - a);
-    console.log(oneArray, maxNumber, minNumber)
+function deleteList() {
+    let itemLi = document.querySelector('ul#list > li:last-child');
+    (itemLi === null) ? alert('Список удален') : itemLi.remove();
 }
 
-transformArray(arr1, arr2);
+//Выводит пустой объект почему-то :(
+localStorage.setItem('list', JSON.stringify(list));
+JSON.parse(localStorage.getItem('list'));
+console.log(JSON.parse(localStorage.getItem('list')));
