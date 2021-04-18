fc3f86a7ceb377dce7f1adbd059a106c32266daa
VeriSkas <92_sveto4ka@mail.ru>
2021-04-18 12:25:32 +0300

    dz16

--- index.js
+++ index.js
@@ -1,182 +1,105 @@
-// ЗАДАНИЕ 1
+// ЗАДАНИЕ 1__________________________________________
 
-let input = document.getElementById('inputValue');
-let changedNum = document.getElementById('changedNumber');
+// let array = [1, 2, 3, 4, 9, 8, 7, 6, 5];
 
-function colonOdd(num) {
-    num = num + input.value;
-    let numArr = num.split('');
-    for(let i = 0; i < numArr.length; i++) {
-        if((numArr[i] % 2 == 1) && (numArr[i + 1] % 2 == 1)) {
-            numArr.splice((i + 1), 0, ':')
-        }
-    }
-    numArr = numArr.join('');
-    changedNum.innerHTML = numArr;
-}
-
-// ЗАДАНИЕ 2 и 3
-
-// let arr = [1, 2, 3, 4, 5, 6, 'hello', 'world'];
-
-// function getFirst(array, n) {
-//     let arrFirst = array.splice(0, n);
-//     console.log(arrFirst);
+// function compareNumbers(arr) {
+//     return arr.sort( (a, b) => b - a);
 // }
 
-// getFirst(arr, 4);
+// console.log(compareNumbers(array));
 
-// function getLast(array, n) {
-//     let arrLast = array.splice(-n);
-//     console.log(arrLast);
+//ЗАДАНИЕ 2_____________________________________________
+
+// let array = [1, 2, null, 3, false, 4, 5, 6, undefined, '', NaN, 7, 0, 8, 9, 'десять'];
+
+// function filterFalse(arr) {
+//     return arr.filter( item =>
+//         item != false && item != undefined && !Number.isNaN(item)
+//     );
 // }
 
-// getLast(arr, 4);
+// console.log(filterFalse(array));
 
-// ЗАДАНИЕ 4
+//ЗАДАНИЕ 3_____________________________________________
 
-// let arr1 = [1, 8, 0, false, 'string', 1, 8, 3, false, 'string', 96, 1, 8, false, 7, 'string', 96, null, 55, '', null, 55, '', 96, null, 55, ''];
-// let arr2 = [14, 55, 'hello', 55, null, 'string', 55, 8, true, 55, 659, 55, null, 'string', 96, 55, 659, true, 55];
-
-// function doUniqueElem(arrey) {
-//     let sortArr = arrey.sort();
-//     for(let i = 0; i < sortArr.length; i++) {
-//         if (sortArr[i] == sortArr[i + 1]) {
-//             let repeatElem = sortArr[i];
-//             while (sortArr.indexOf(repeatElem) != -1) {
-//                 sortArr.splice((sortArr.indexOf(repeatElem)), 1)
-//                 i--;
-//             }
-//         }
-//     }
-//     return arrey;
+// let user1 = {
+//     firstName: 'Vasya',
+//     lastName: 'Petrova',
+//     age: 22
 // }
-
-// function concatArrey(arrey1, arrey2) {
-//     let arreyOne = doUniqueElem(arrey1);
-//     let arreyTwo = doUniqueElem(arrey2);
-//     let concatArrey = arreyOne.concat(arreyTwo);
-//     console.log(concatArrey);
+// let user2 = {
+//     firstName: 'Gena',
+//     lastName: 'Sidorov',
+//     age: 28
 // }
-
-// concatArrey(arr1, arr2);
-
-
-// ЗАДАНИЕ 5
-
-// let arrey = [5, 69, '63fghbrft', '55grger', 20];
-// let elemSqr = 0;
-
-// function sumOfSquares(arr) {
-//     for(let i = 0; i < arr.length; i++) {
-//         elemSqr = elemSqr + Math.pow(parseFloat(arr[i]), 2);
-//     }
-//     console.log(elemSqr);
+// let user3 = {
+//     firstName: 'Lena',
+//     lastName: 'Ivanova',
+//     age: 44
 // }
-
-// sumOfSquares(arrey);
-
-
-// ЗАДАЧКА В КЛАССЕ (преобразовать в массив с массивами)
-
-// let user = [
-//     {
-//     name: 'Vasya',
-//     age: 21
-//     },
-//     {
-//     name: 'Vanya',
-//     age: 18
-//     },
-//     {
-//     name: 'Petya',
-//     age: 16
-//     },
-//     {
-//     name: 'Katya',
-//     age: 29,
-//     },
-// ]
-// let usersMainArr = [];
-
-// function arrUsers (arr) {
-
-//     for(let i = 0; i < arr.length; i++) {
-//         let arrUser = [];
-//         for(let key in arr[i]) {
-//             arrUser.push(arr[i][key]);
-//         }
-//         usersMainArr[i] = arrUser;
-//     }
-//     console.log(usersMainArr);
+// let user4 = {
+//     firstName: 'Katya',
+//     lastName: 'Pupkina',
+//     age: 20
 // }
-
-// arrUsers (user);
-
-// ЗАДАНИЕ 6
-
-// let user = [
-//     {
-//     name: 'Vasya',
-//     age: 21
-//     },
-//     {
-//     name: 'Vanya',
-//     age: 18
-//     },
-//     {
-//     name: 'Petya',
-//     age: 16
-//     },
-//     {
-//     name: 'Katya',
-//     age: 29,
-//     },
-// ]
-
-// function sortArr(arr) {
-//     for(let j = 0; j  < arr.length - 1; j++) {
-//         for(let i = 0; i < arr.length - 1; i++) {
-//             if(arr[i].age > arr[i + 1].age) {
-//                 let tempValue = arr[i];
-//                 arr[i] = arr[i + 1];
-//                 arr[i + 1] = tempValue;
-//             }
-//         }
-//     }
-//     console.log(arr);
-// }
-
-// sortArr(user);
-
-
-// ЗАДАНИЕ 7
-
-// let user = [
-//     {
-//     name: 'Vasya',
-//     age: 21
-//     },
-//     {
-//     name: 'Vanya',
-//     age: 18
-//     },
-//     {
-//     name: 'Petya',
-//     age: 16
-//     },
-//     {
-//     name: 'Katya',
-//     age: 29,
-//     },
-// ]
+// let users = [user1, user2, user3, user4];
 
 // function propertyValue(array, key) {
-//     let keyValue = [];
-//     for(let i = 0; i < array.length; i++) {
-//         keyValue.push(array[i][key]);
-//     }
-//     console.log(keyValue);
+//     return array.map( item => item[key]);
 // }
 
-// propertyValue(user, "name");
+// console.log(propertyValue(users, "lastName"));
+
+//ЗАДАНИЕ 4_______________________________________
+
+// let arrStr = ['Это', 'есть', 'массив', 'строк'];
+// let arrLength = arrStr.map( item => item.length);
+// console.log(arrLength);
+
+//ЗАДАНИЕ 5_________________________________________
+
+// let numbers = [2, 3, 5, 7, 11, 13, 17, 19];
+// let tempValue = 0;
+
+// function currentSums(arr) {
+//     let summ = arr.reduce( (temp, item) => {
+//         tempValue = tempValue + item;
+//         return [...temp, tempValue];
+//     }, [])
+//     console.log(summ);
+// }
+
+// currentSums(numbers);
+
+//ЗАДАНИЕ 6_____________________________________
+
+// let arrStr = ['Это', 'есть', 'массив', 'строк'];
+// let arrLength = arrStr.reduce( (temp, item) => {
+//     return [...temp, item.length]
+// }, []);
+// console.log(arrLength);
+
+//ЗАДАНИЕ 7________________________________________
+
+let inputValue = document.getElementById('inputValue');
+let outputValue = document.getElementById('outputValue');
+let btnOk = document.getElementById('btnOk');
+let btnOut = document.getElementById('btnOut');
+let arr = [];
+
+btnOk.onclick = function() {
+    arr.push(inputValue.value);
+    inputValue.value = '';
+    console.log(arr);
+}
+
+function sortNumber(arrey) {
+    return arrey.sort( (a, b) => a - b);
+}
+
+btnOut.onclick = function() {
+    arr = sortNumber(arr);
+    outputValue.innerHTML = arr.join('; ');
+    arr = [];
+}
+
