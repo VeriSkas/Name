54a225e11330b3d87139ef6e601f4b35c20a62a1
VeriSkas <92_sveto4ka@mail.ru>
2021-04-19 18:20:10 +0300

    dz14

---
+++ index1.js
@@ -0,0 +1,128 @@
+// _______________1_________________
+
+// let arr = [1, 2, 3, 4, 5,];
+// arr.splice(1, 2);
+// console.log(arr);
+
+// _______________2_________________
+
+// let str = 'My favorite "Hello world"';
+// let arr = str.split('').reverse();
+// let reverseStr = arr.join('');
+// console.log(reverseStr);
+
+// ______________3__________________
+
+// let str = 'My favorite "Hello world"';
+// let arr = str.split('');
+// let reverseArr = [];
+
+// for (i = arr.length - 1; i >= 0; i--) {
+//     reverseArr.push(arr[i]);
+// }
+
+// let reverseStr = reverseArr.join('');
+// console.log(reverseStr);
+
+// ______________4____________________
+
+// let arr = ['Hello', 4, 99, 0, false, 'world', null];
+// let arr1 = ['Hello', 4, 99, 0, false, 'world', null];
+// let arr2 = ['Hello', 4, 100, 0, true, 'world', null, ''];
+
+// function compareArr (a, b) {
+//     if (a.length === b.length) {
+//         for (i = 0; i < a.length; i++){
+//             if  (a[i] !== b[i]) {
+//                 console.log('false');
+//                 return false;
+//             }
+//         }
+//         console.log('true');
+//         return true;
+//     } else {
+//         console.log('false');
+//         return false;
+//     }
+// }
+
+// compareArr(arr, arr1);
+// compareArr(arr, arr2);
+
+// _________________5___________________
+
+// for (i = 1; i <= 100; i++) {
+//     if ((i % 3 === 0) && (i % 5 === 0)) {
+//         console.log('FizzBuzz');
+//     } else if (i % 3 === 0) {
+//         console.log('Fizz');
+//     } else if (i % 5 === 0) {
+//         console.log('Buzz');
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+// _________________6____________________
+
+// let arr = [];
+// let arr1 = [];
+// let lengthArr = 8;
+
+// for (i = 0; i < lengthArr; i++) {
+//     if (i % 2 === 0) {
+//         arr[i] = '#';
+//         arr1[i] = ' ';
+//     } else {
+//         arr[i] = ' ';
+//         arr1[i] = '#';
+//     }
+// }
+
+// for (i = 0; i < lengthArr; i++) {
+//     (i % 2 === 0) ? console.log(arr.join('')): console.log(arr1.join(''));
+// }
+
+// _________________7_________________
+
+let input = document.getElementById('input');
+let list = document.getElementById('list');
+let liItems = document.getElementsByTagName('li');
+
+
+function inputList() {
+    let newListItem = document.createElement('li');
+    newListItem.setAttribute("id", "li" + list.children.length);
+    newListItem.textContent = input.value;
+    list.appendChild(newListItem);
+    input.value = '';
+}
+
+function deleteList() {
+    let itemLi = document.querySelector('ul#list > li:last-child');
+    (itemLi === null) ? alert('Список удален') : itemLi.remove();
+}
+
+list.onclick = function(event) {
+    let tag = event.target;
+    if(tag.tagName == "LI") {
+        tag.remove();
+    }
+}
+
+function saveList() {
+    let li = [];
+    for(i = 0; i < liItems.length; i++) {
+        li[i] = liItems[i].innerText;
+    }
+
+    localStorage.setItem('li', JSON.stringify(li));
+}
+
+function openSaveList() {
+    for(i = 0; i < JSON.parse(localStorage.getItem('li')).length; i++) {
+        let saveListItem = document.createElement('li');
+        saveListItem.textContent = JSON.parse(localStorage.getItem('li'))[i];
+        list.appendChild(saveListItem);
+    }
+}
