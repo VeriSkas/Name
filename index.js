b633f0c51cc5643003b35aeb1bf71b3a8816e4ed
VeriSkas <92_sveto4ka@mail.ru>
2021-04-22 16:13:19 +0300

    dz17

--- index.js
+++ index.js
@@ -1,105 +1,147 @@
-// ЗАДАНИЕ 1__________________________________________
+// ЗАДАНИЕ 1_________________________________
 
-// let array = [1, 2, 3, 4, 9, 8, 7, 6, 5];
+class Worker {
+    constructor(name, surname, rate, days) {
+        this.name = name;
+        this.surname = surname;
+        this.rate = rate;
+        this.days = days;
+    }
 
-// function compareNumbers(arr) {
-//     return arr.sort( (a, b) => b - a);
-// }
-
-// console.log(compareNumbers(array));
-
-//ЗАДАНИЕ 2_____________________________________________
-
-// let array = [1, 2, null, 3, false, 4, 5, 6, undefined, '', NaN, 7, 0, 8, 9, 'десять'];
-
-// function filterFalse(arr) {
-//     return arr.filter( item =>
-//         item != false && item != undefined && !Number.isNaN(item)
-//     );
-// }
-
-// console.log(filterFalse(array));
-
-//ЗАДАНИЕ 3_____________________________________________
-
-// let user1 = {
-//     firstName: 'Vasya',
-//     lastName: 'Petrova',
-//     age: 22
-// }
-// let user2 = {
-//     firstName: 'Gena',
-//     lastName: 'Sidorov',
-//     age: 28
-// }
-// let user3 = {
-//     firstName: 'Lena',
-//     lastName: 'Ivanova',
-//     age: 44
-// }
-// let user4 = {
-//     firstName: 'Katya',
-//     lastName: 'Pupkina',
-//     age: 20
-// }
-// let users = [user1, user2, user3, user4];
-
-// function propertyValue(array, key) {
-//     return array.map( item => item[key]);
-// }
-
-// console.log(propertyValue(users, "lastName"));
-
-//ЗАДАНИЕ 4_______________________________________
-
-// let arrStr = ['Это', 'есть', 'массив', 'строк'];
-// let arrLength = arrStr.map( item => item.length);
-// console.log(arrLength);
-
-//ЗАДАНИЕ 5_________________________________________
-
-// let numbers = [2, 3, 5, 7, 11, 13, 17, 19];
-// let tempValue = 0;
-
-// function currentSums(arr) {
-//     let summ = arr.reduce( (temp, item) => {
-//         tempValue = tempValue + item;
-//         return [...temp, tempValue];
-//     }, [])
-//     console.log(summ);
-// }
-
-// currentSums(numbers);
-
-//ЗАДАНИЕ 6_____________________________________
-
-// let arrStr = ['Это', 'есть', 'массив', 'строк'];
-// let arrLength = arrStr.reduce( (temp, item) => {
-//     return [...temp, item.length]
-// }, []);
-// console.log(arrLength);
-
-//ЗАДАНИЕ 7________________________________________
-
-let inputValue = document.getElementById('inputValue');
-let outputValue = document.getElementById('outputValue');
-let btnOk = document.getElementById('btnOk');
-let btnOut = document.getElementById('btnOut');
-let arr = [];
-
-btnOk.onclick = function() {
-    arr.push(inputValue.value);
-    inputValue.value = '';
-    console.log(arr);
+    getSalary() {
+        return this.rate*this.days;
+    }
 }
 
-function sortNumber(arrey) {
-    return arrey.sort( (a, b) => a - b);
-}
+const worker1 = new Worker('Vasya', 'Pypkin', 50, 20);
+console.log(worker1);
+console.log(worker1.getSalary());
 
-btnOut.onclick = function() {
-    arr = sortNumber(arr);
-    outputValue.innerHTML = arr.join('; ');
-    arr = [];
-}
+//ЗАДАНИЕ 2________________________________________
 
+// class Worker {
+//     #name = 'Vasya';
+//     #surname = 'Pypkin';
+//     #rate = 50;
+//     #days = 20;
+
+//     get name() {
+//         return this.#name;
+//     }
+
+//     get surname() {
+//         return this.#surname;
+//     }
+
+//     get rate() {
+//         return this.#rate;
+//     }
+
+//     get days() {
+//         return this.#days;
+//     }
+
+//     #getSalary() {
+//         return this.#rate * this.#days;
+//     }
+
+//     get getSalary() {
+//         return this.#getSalary();
+//     }
+
+// }
+
+// const worker1 = new Worker();
+// console.log(worker1);
+// console.log(worker1.getSalary);
+
+//ЗАДАНИЕ 3______________________________________
+
+// class Worker {
+//     #name = 'Vasya';
+//     #surname = 'Pypkin';
+//     #rate = 50;
+//     #days = 20;
+
+//     get name() {
+//         return this.#name;
+//     }
+
+//     get surname() {
+//         return this.#surname;
+//     }
+
+//     get rate() {
+//         return this.#rate;
+//     }
+
+//     set rate(value) {
+//         value = parseInt(value);
+//         if(value > 20) {
+//             this.#rate = value;
+//         } else {
+//             console.log('За копейки никто работать не будет!');
+//         }
+//     }
+
+//     get days() {
+//         return this.#days;
+//     }
+
+//     set days(value) {
+//         value = parseInt(value);
+//         if(value < 0 || value > 20) {
+//             console.log('Недопустимое количество рабочих дней');
+//         } else {
+//             this.#days = value;
+//         }
+//     }
+
+//     #getSalary() {
+//         return this.#rate * this.#days;
+//     }
+
+//     get getSalary() {
+//         return this.#getSalary();
+//     }
+
+// }
+
+// const worker1 = new Worker();
+// worker1.rate = 30;
+// console.log(worker1.rate);
+// worker1.days = -10;
+// console.log(worker1.days);
+// console.log(worker1.getSalary);
+
+//ЗАДАНИЕ 4________________________________
+
+// class Rectangle {
+//     constructor(length, width) {
+//         this.length = length;
+//         this.width = width;
+//     }
+
+//     getSquare() {
+//         return this.length * this.width;
+//     }
+// }
+
+// class Parallelepiped extends Rectangle {
+//     constructor(height, length, width) {
+//         super(length, width);
+//         this.height = height;
+//     }
+
+//     getSquare() {
+//         return this.length * this.width * this.height;
+//     }
+// }
+
+// const rect1 = new Rectangle(200, 100);
+// console.log(rect1);
+// console.log(rect1.getSquare());
+
+// const paral1 = new Parallelepiped(50, 200, 100);
+// console.log(paral1);
+// console.log(paral1.getSquare());
