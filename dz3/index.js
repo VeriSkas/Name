ede661bdfda0b5aba879540c19d2dcd4e75d6cca
Светлана Веракса <92_sveto4ka@mail.ru>
2021-06-10 10:59:18 +0300

    10.06

--- dz3/index.js
+++ dz3/index.js
@@ -1,65 +1,83 @@
-class Human {
-    constructor(name, surname, age) {
-        this.name = name;
-        this.surname = surname;
-        this.age = age;
-    };
+// class Human {
+//     constructor(name, surname, age) {
+//         this.name = name;
+//         this.surname = surname;
+//         this.age = age;
+//     };
 
-    #getFullName() {
-        return `${this.name} ${this.surname}`;
-    };
+//     #getFullName() {
+//         return `${this.name} ${this.surname}`;
+//     };
 
-    get getFullName() {
-        return this.#getFullName;
-    }
+//     get getFullName() {
+//         return this.#getFullName;
+//     }
 
-};
+// };
 
-class Worker extends Human {
-    #rate = 100;
-    constructor(name, surname, age, days) {
-        super(name, surname, age);
-        this.days = days;
-    };
+// class Worker extends Human {
+//     #rate = 100;
+//     constructor(name, surname, age, days) {
+//         super(name, surname, age);
+//         this.days = days;
+//     };
 
-    get rate() {
-        return this.#rate;
-    };
+//     get rate() {
+//         return this.#rate;
+//     };
 
-    #getSalary() {
-        return this.#rate * this.days;
-    };
+//     #getSalary() {
+//         return this.#rate * this.days;
+//     };
 
-    get getSalary() {
-        return this.#getSalary;
-    }
+//     get getSalary() {
+//         return this.#getSalary;
+//     }
 
-}
+// }
 
-class Developer extends Human {
-    constructor(name, surname, age, sphere, company) {
-        super(name, surname, age);
-        this.sphere = sphere;
-        this.company = company;
-    };
-}
+// class Developer extends Human {
+//     constructor(name, surname, age, sphere, company) {
+//         super(name, surname, age);
+//         this.sphere = sphere;
+//         this.company = company;
+//     };
+// }
 
-class Designer extends Human {
-    constructor(name, surname, age, platform, environment) {
-        super(name, surname, age);
-        this.platform = platform;
-        this.environment = environment;
-    }
-}
+// class Designer extends Human {
+//     constructor(name, surname, age, platform, environment) {
+//         super(name, surname, age);
+//         this.platform = platform;
+//         this.environment = environment;
+//     }
+// }
 
-const vasya = new Human('Vasya', 'Pupkin', 45);
-console.log(vasya.getFullName());
+// const vasya = new Human('Vasya', 'Pupkin', 45);
+// console.log(vasya.getFullName());
 
-const worker = new Worker('Vasya', 'Pupkin', 45, 15);
-console.log(worker.getSalary());
+// const worker = new Worker('Vasya', 'Pupkin', 45, 15);
+// console.log(worker.getSalary());
 
-const dev = new Developer('Ivan', 'Ivanov', 30, 'FrontendDev', 'ITcomp');
-console.log(dev.getFullName());
+// const dev = new Developer('Ivan', 'Ivanov', 30, 'FrontendDev', 'ITcomp');
+// console.log(dev.getFullName());
 
-const designer = new Designer('Anton', 'Sidorov', 40, 'Photoshop', 'Web');
-console.log(designer.getFullName());
+// const designer = new Designer('Anton', 'Sidorov', 40, 'Photoshop', 'Web');
+// console.log(designer.getFullName());
+
+
+// class MyPromise {
+//     constructor(func) {
+//         this.theN = func;
+//     }
+// };
+
+// const myResolve = word => {
+//     console.log('resolve', word);
+// };
+
+// const promis = new MyPromise( myResolve => {
+//     const str = 'hello';
+//     myResolve(str);
+// });
+
+// promis.theN(myResolve);
